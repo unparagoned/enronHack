62ac721e12a4d735b0c2348c9c8ae0be254c9fa6
Data Exploitation Mac 1 <dataexploitationmac1@Data-Exploitations-MacBook-Pro-1.local>
2018-04-10 16:50:37 +0100

    updated script

--- Back/parsing_emails.py
+++ Back/parsing_emails.py
@@ -4,11 +4,15 @@ from email.parser import Parser
 # import data
 emails = pd.read_csv('emails.csv')
 
-# parse each email and extract fields
+# Parse each field from the full enron dataset, takes too long to run from local machine
 for index, email in emails.iterrows():
+    if pd.notnull(email['from']) == True:
+        continue
     parsed = Parser().parsestr(email.message)
     emails.loc[index,'date'] = parsed['Date']
     emails.loc[index,'from'] = parsed['From']
     emails.loc[index,'to'] = parsed['To']
     emails.loc[index,'subject'] = parsed['Subject']
     emails.loc[index,'body'] = parsed.get_payload()
+    if (index % 100) == 0:
+        print(index)
